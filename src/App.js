f341657ce329a75bcc77efe75da1b872a2964269
techsithgit <hemilp@tuplecorp.com>
2019-07-07 15:23:45 +0100

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+
+const initXY = {
+  x: null,
+  y: null
+};
 
 function App() {
+  const [time, setTime] = useState(Date);
+  const [xy, setXY] = useState(initXY);
+
+  useEffect(() => {
+    let handle = setInterval(() => {
+      setTime(Date);
+    }, 3000);
+
+    return () => {
+      clearInterval(handle);
+    };
+  });
+
+  function mousemoveHandle(e) {
+    setXY({
+      x: e.clientX,
+      y: e.clientY
+    });
+  }
+
+  useEffect(() => {
+    window.addEventListener("mousemove", mousemoveHandle);
+    return () => {
+      window.removeEventListener("mousemove", mousemoveHandle);
+    };
+  });
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>Use Effect Examples</h2>
+        <h3>Date & Time : {time}</h3>
+        <h3>{`x:${xy.x}, y:${xy.y}`}</h3>
       </header>
     </div>
   );
